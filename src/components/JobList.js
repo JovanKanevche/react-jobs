6339a764333511c241bef2ea0ee3e1db77d2ba69
JovanKanevche <kanevche.jovan@gmail.com>
2020-09-02 04:43:05 +0200

    Add Job Component

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,7 +1,17 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { useQuery } from "@apollo/react-hooks";
 import { QUERY_JOBS } from "../queries/Jobs";
+import { Card } from "antd";
+import styled from "styled-components";
+
+const Job = ({ id, title, city, investors }) => {
+  return (
+    <Card key={id} title={title} bordered={true}>
+      <p>{city}</p>
+      <p>{investors}</p>
+    </Card>
+  );
+};
 
 const JobList = (props) => {
   const { loading, error, data } = useQuery(QUERY_JOBS);
@@ -9,13 +19,27 @@ const JobList = (props) => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
 
-  return data.jobs.map(({ id, title, city }) => (
-    <div key={id}>
-      <p>{title}</p>
-    </div>
-  ));
+  return (
+    <AppContainer>
+      <GridComponent>
+        {data.jobs.map(({ id, title, city }) => (
+          <Job key={id} id={id} title={title} city={city} investors="" />
+        ))}
+      </GridComponent>
+    </AppContainer>
+  );
 };
 
-JobList.propTypes = {};
+const GridComponent = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
+  max-width: 1024px;
+`;
+
+const AppContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default JobList;
